d041eb1b1ccb8d6d0b318a0c44ca3987d4207476
Vivek Singh Rajput <vivekcatqua309@gmail.com>
2023-06-25 22:17:30 +0530

    Add files via upload

---
+++ App.py
@@ -0,0 +1,142 @@
+from dash import Dash, html, dcc, Input, Output, callback
+import pandas as pd
+import plotly.express as px
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+
+df = pd.read_csv('https://raw.githubusercontent.com/Vivek-84/Dashd/main/data11a.csv')
+
+
+app.layout = html.Div([
+    html.Div([
+
+        html.Div([
+            dcc.Dropdown(
+                df['Sector'].unique(),
+                'Sec119 Noida',
+                id='crossfilter-xaxis-column',
+            ),
+            dcc.RadioItems(
+                ['Linear', 'Log'],
+                'Linear',
+                id='crossfilter-xaxis-type',
+                labelStyle={'display': 'inline-block', 'marginTop': '5px'}
+            )
+        ],
+        style={'width': '49%', 'display': 'inline-block'}),
+
+        html.Div([
+            dcc.Dropdown(
+                df['Sector'].unique(),
+                'Sec128 Noida',
+                id='crossfilter-yaxis-column'
+            ),
+            dcc.RadioItems(
+                ['Linear', 'Log'],
+                'Linear',
+                id='crossfilter-yaxis-type',
+                labelStyle={'display': 'inline-block', 'marginTop': '5px'}
+            )
+        ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
+    ], style={
+        'padding': '10px 5px'
+    }),
+
+    html.Div([
+        dcc.Graph(
+            id='crossfilter-indicator-scatter',
+            hoverData={'points': [{'customdata': '1 BHK'}]}
+        )
+    ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),
+    html.Div([
+        dcc.Graph(id='x-time-series'),
+        dcc.Graph(id='y-time-series'),
+    ], style={'display': 'inline-block', 'width': '49%'}),
+
+    html.Div(dcc.Slider(
+        df['Year'].min(),
+        df['Year'].max(),
+        step=None,
+        id='crossfilter-year--slider',
+        value=df['Year'].max(),
+        marks={str(year): str(year) for year in df['Year'].unique()}
+    ), style={'width': '49%', 'padding': '0px 20px 20px 20px'})
+])
+
+
+@callback(
+    Output('crossfilter-indicator-scatter', 'figure'),
+    Input('crossfilter-xaxis-column', 'value'),
+    Input('crossfilter-yaxis-column', 'value'),
+    Input('crossfilter-xaxis-type', 'value'),
+    Input('crossfilter-yaxis-type', 'value'),
+    Input('crossfilter-year--slider', 'value'))
+def update_graph(xaxis_column_name, yaxis_column_name,
+                 xaxis_type, yaxis_type,
+                 year_value):
+    dff = df[df['Year'] == year_value]
+
+    fig = px.scatter(x=dff[dff['Sector'] == xaxis_column_name]['Price'],
+            y=dff[dff['Sector'] == yaxis_column_name]['Price'],
+            hover_name=dff[dff['Sector'] == yaxis_column_name]['Type']
+            )
+
+    fig.update_traces(customdata=dff[dff['Sector'] == yaxis_column_name]['Type'])
+
+    fig.update_xaxes(title=xaxis_column_name, type='linear' if xaxis_type == 'Linear' else 'log')
+
+    fig.update_yaxes(title=yaxis_column_name, type='linear' if yaxis_type == 'Linear' else 'log')
+
+    fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
+
+    return fig
+
+
+def create_time_series(dff, axis_type, title):
+
+    fig = px.scatter(dff, x='Year', y='Price')
+
+    fig.update_traces(mode='lines+markers')
+
+    fig.update_xaxes(showgrid=False)
+
+    fig.update_yaxes(type='linear' if axis_type == 'Linear' else 'log')
+
+    fig.add_annotation(x=0, y=0.85, xanchor='left', yanchor='bottom',
+                       xref='paper', yref='paper', showarrow=False, align='left',
+                       text=title)
+
+    fig.update_layout(height=225, margin={'l': 20, 'b': 30, 'r': 10, 't': 10})
+
+    return fig
+
+
+@callback(
+    Output('x-time-series', 'figure'),
+    Input('crossfilter-indicator-scatter', 'hoverData'),
+    Input('crossfilter-xaxis-column', 'value'),
+    Input('crossfilter-xaxis-type', 'value'))
+def update_y_timeseries(hoverData, xaxis_column_name, axis_type):
+    type_ = hoverData['points'][0]['customdata']
+    dff = df[df['Type'] == type_]
+    dff = dff[dff['Sector'] == xaxis_column_name]
+    title = '<b>{}</b><br>{}'.format(type_, xaxis_column_name)
+    return create_time_series(dff, axis_type, title)
+
+
+@callback(
+    Output('y-time-series', 'figure'),
+    Input('crossfilter-indicator-scatter', 'hoverData'),
+    Input('crossfilter-yaxis-column', 'value'),
+    Input('crossfilter-yaxis-type', 'value'))
+def update_x_timeseries(hoverData, yaxis_column_name, axis_type):
+    dff = df[df['Type'] == hoverData['points'][0]['customdata']]
+    dff = dff[dff['Sector'] == yaxis_column_name]
+    return create_time_series(dff, axis_type, yaxis_column_name)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=False)
